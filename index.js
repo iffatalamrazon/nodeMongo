f0b5d687eba4decb52fb3ef258982a6ba1da7d38
iffatalamrazon <iffatalamrazon100u@gmail.com>
2021-03-26 23:01:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express =require('express');
+const app =express();
+app.get('/',(req, res) =>{res.send ("Thank you so much")});
+
+
+app.listen(3000 , () => console.log ('listening to  port 3000 '));
